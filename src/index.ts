7193c6bab0a59659f1302c7ad7d59cc9d769588c
Azury <azury.wtf@gmail.com>
2022-07-09 18:07:29 +0200

    added main file

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Application, Router } from 'https://deno.land/x/oak@v10.6.0/mod.ts'
+
+const app = new Application()
+, router = new Router()
+
+router.get('/', (ctx) => {
+  ctx.response.body = 'Hello World'
+})
+
+app.use(router.allowedMethods())
+app.use(router.routes())
+
+app.use(ctx => {
+  ctx.response.status = 404
+  ctx.response.body = 'Not Found'
+})
+
+await app.listen({
+  port: 8000
+})
